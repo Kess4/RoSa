99889041a8a065d48f5c88645e1bbcf402bcdcd1
Kess4 <kessia.tona@epitech.digital>
2023-11-20 18:14:53 +0100

    first commit/first page

---
+++ front/screens/tuto.jsx
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, } from 'react-native';
+import { NativeBaseProvider, Button, Image, Svg} from "native-base";
+
+// import Svg from 'react-native-svg';
+
+export default function App() {
+  return (
+    <NativeBaseProvider>
+    <View style={{width: '100%', height: '100%', paddingLeft: 43, paddingRight: 43, paddingTop: 187, paddingBottom: 187, background: 'white', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', gap: 35, display: 'inline-flex'}}>
+      <View style={{paddingBottom: 10, flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', gap: 30, display: 'flex'}}>
+        <Text style={{textAlign: 'justify', color: '#EB3B5A', fontSize: 40, fontWeight: '800', letterSpacing: 16, wordWrap: 'break-word'}}>ROSA</Text>
+      </View>
+      <View style={{width: 240, height: 276.88, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+        <Image marginLeft={'auto'} marginRight={'auto'} source={require('../assets/logo.png')} alt="Logo"/>
+      </View>
+        <Button style={{width: '150px', height: '50px', paddingLeft: 26, paddingRight: 26, paddingTop: 14, paddingBottom: 14, background: '#3867D6', borderRadius: 10, justifyContent: 'center', alignItems: 'center', gap: 10, display: 'inline-flex'}}>
+          <Text style={{textAlign: 'justify', color: 'white', fontSize: 17, fontWeight: '700', wordWrap: 'break-word'}}>Commencer</Text> 
+        </Button>     
+    </View>
+    </NativeBaseProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
